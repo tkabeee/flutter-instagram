1473876d807e54638fbe3557e26bd2d3cf1cc31e
Takayuki Kondo <tkabeee@users.noreply.github.com>
2023-06-07 21:00:10 +0900

    Update auth_methods.dart

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,8 +1,11 @@
 // import 'dart:typed_data';
 
+import 'dart:typed_data';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram/resources/storage_methods.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -14,7 +17,7 @@ class AuthMethods {
     required String password,
     required String username,
     required String bio,
-    // required Uint8List file,
+    required Uint8List file,
   }) async {
     String res = "Some error occurred";
     try {
@@ -27,6 +30,10 @@ class AuthMethods {
             email: email, password: password);
 
         print(cred.user!.uid);
+
+        String photoUrl = await StorageMethods()
+            .uploadImageToStorage('profilePics', file, false);
+
         // add user to database
         await _firestore.collection('users').doc(cred.user!.uid).set({
           'username': username,
@@ -35,6 +42,7 @@ class AuthMethods {
           'bio': bio,
           'followers': [],
           'following': [],
+          'photoUrl': photoUrl,
         });
         res = "Success";
       }
